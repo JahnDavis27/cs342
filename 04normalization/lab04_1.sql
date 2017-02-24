e2e1d35e87a5898a6a521b37ecbfe7dfc38d5ac2
Jahnathan Davis <jrd58@calvin.edu>
2017-02-24 12:26:55 -0500

    Lab04

---
+++ 04normalization/lab04_1.sql
@@ -0,0 +1,49 @@
+-- This command file loads an experimental person relation.
+-- The data conforms to the following assumptions:
+--     * Person IDs and team names are unique for people and teams respectively.
+--     * People can have at most one mentor.
+--     * People can be on many teams, but only have one role per team.
+--     * Teams meet at only one time.
+--
+-- CS 342
+-- Spring, 2017
+-- Jahn Davis
+
+drop table AltPerson;
+
+CREATE TABLE AltPerson (
+	personId integer,
+	name varchar(10),
+	status char(1),
+	mentorId integer,
+	mentorName varchar(10),
+	mentorStatus char(1),
+    teamName varchar(10),
+    teamRole varchar(10),
+    teamTime varchar(10)
+	);
+
+INSERT INTO AltPerson VALUES (0, 'Ramez', 'v', 1, 'Shamkant', 'm', 'elders', 'trainee', 'Monday');
+INSERT INTO AltPerson VALUES (1, 'Shamkant', 'm', NULL, NULL, NULL, 'elders', 'chair', 'Monday');
+INSERT INTO AltPerson VALUES (1, 'Shamkant', 'm', NULL, NULL, NULL, 'executive', 'protem', 'Wednesday');
+INSERT INTO AltPerson VALUES (2, 'Jennifer', 'v', 3, 'Jeff', 'm', 'deacons', 'treasurer', 'Tuesday');
+INSERT INTO AltPerson VALUES (3, 'Jeff', 'm', NULL, NULL, NULL, 'deacons', 'chair', 'Tuesday');
+
+-------------------------------------------------------------------------------------------------------------
+--Exercise 4.1a
+	--This schema violates a lot of the requirements for various levels of normalized form. There are a disgusting amount of NULL values in this relation.
+		-- A lot of these attributes should be in their own separate tables. The lack of primary and foreign keys is incredibly disturbing. There will be no 
+		-- way to uniquely identify ANY records, potentially resulting in duplicate records, wasting database space. 
+	--We must determine whether or not this relation is in BCNF or 4NF.
+		--All the left hand sides of the non-trivial functional dependencies must be a superkey of the relation. What we see here is not the case at all. There
+			-- is no mechanism in place that confirms that the superkey uniquely identifies the attributes of a relation. Thus, this requirement is violated. 
+			-- We also see that the only real primary key is (personId, teamId). But personId functionally defines name, thus violating BCNF requirements.
+			
+--Exercise 4.1b
+	--Properly normalized schema:
+		--Person(personId, name, status, mentorId)
+		--Team(teamId, teamName, role, time)
+		--Person_Team(personId, teamId)
+		
+	--This schema is in BCNF. For each functional dependency in the table, the left hand side of each f.d. is a superkey for the relation.		
+		
---
+++ 04normalization/lab04_2.sql
@@ -0,0 +1,47 @@
+-- This command file loads an experimental person database.
+-- The data conforms to the following assumptions:
+--     * People can have 0 or many team assignments.
+--     * People can have 0 or many visit dates.
+--     * Teams and visits don't affect one another.
+--
+-- CS 342, Spring, 2017
+-- Jahn Davis
+
+DROP TABLE PersonTeam;
+DROP TABLE PersonVisit;
+
+CREATE TABLE PersonTeam (
+	personName varchar(10),
+    teamName varchar(10)
+	);
+
+CREATE TABLE PersonVisit (
+	personName varchar(10),
+    personVisit date
+	);
+
+-- Load records for two team memberships and two visits for Shamkant.
+INSERT INTO PersonTeam VALUES ('Shamkant', 'elders');
+INSERT INTO PersonTeam VALUES ('Shamkant', 'executive');
+INSERT INTO PersonVisit VALUES ('Shamkant', '22-FEB-2015');
+INSERT INTO PersonVisit VALUES ('Shamkant', '1-MAR-2015');
+
+-- Query a combined "view" of the data of the form View(name, team, visit).
+SELECT pt.personName, pt.teamName, pv.personVisit
+FROM PersonTeam pt, PersonVisit pv
+WHERE pt.personName = pv.personName;
+
+------------------------------------------------------------------------------------------------------
+--Exercise 4.2a
+	--The relations in this table are technically in both BCNF and 4NF. How can we see this? The relations once again have no primary keys, and also have no functional dependencies.
+		--Because there are no functional dependencies to violate, this relation is all-key. Thus, it is automatically in BCNF. There are also no multivalued dependencies, so the relation
+		--is in 4NF as well. Because there isn't a situation where an attribute defines another with respect to a third attribute, there are no multivalued dependencies.
+
+--Exercise 4.2b
+	--This view is in BCNF, as all of these attributes are necessary to uniquely identify the relations. There are no functional dependencies present in this relaion, because no attribute
+		-- is functionally dependpent upon another attribute. However, this relation is not in 4NF. There is a multivalued dependency where personName identifies teamName and personVisit. Thus,
+		-- the multivalued dependency rule is violated, as the left side is NOT a superkey for the relation. 
+
+--Exercise 4.2c
+	--Just because the number of views and records are the same, this does not mean that they are equally appropriate. The single view needs to have all the records to account for all
+	--of the other information. 
